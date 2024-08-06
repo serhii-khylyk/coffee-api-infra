e65a56a7271d1c7f633eb0f2a1184540830a5aeb
Serhii Khylyk <sergey.hilik@gmail.com>
2024-08-06 12:15:17 +0200

    fix: format code

--- github-repo/main.tf
+++ github-repo/main.tf
@@ -1,39 +1,39 @@
 resource "github_repository" "coffee_api" {
-    name        = var.github_reponame
-    description = "Coffee API repo"
+  name        = var.github_reponame
+  description = "Coffee API repo"
 
-    visibility = "public"
-    gitignore_template = "Python"
+  visibility         = "public"
+  gitignore_template = "Python"
 
 }
 
 resource "github_branch" "all" {
   repository = github_repository.coffee_api.name
-  count = length(var.github_branches)
+  count      = length(var.github_branches)
   branch     = var.github_branches[count.index]
 }
 
 
-resource "github_branch_default" "default"{
+resource "github_branch_default" "default" {
   repository = github_repository.coffee_api.name
   branch     = var.github_branches[0]
 }
 
 resource "github_branch_protection" "branches_protect" {
   repository_id = github_repository.coffee_api.name
-  count = length(var.github_branches)
-  pattern = var.github_branches[count.index]
+  count         = length(var.github_branches)
+  pattern       = var.github_branches[count.index]
 
 }
 
 resource "github_actions_secret" "pg_password_secret" {
-  repository       = github_repository.coffee_api.name
-  secret_name      = "PG_PASSWORD"
-  plaintext_value  = var.pg_password
+  repository      = github_repository.coffee_api.name
+  secret_name     = "PG_PASSWORD"
+  plaintext_value = var.pg_password
 }
 
 resource "github_actions_variable" "pg_username_variable" {
-  repository       = github_repository.coffee_api.name
-  variable_name    = "PG_USERNAME"
-  value            = var.pg_username
+  repository    = github_repository.coffee_api.name
+  variable_name = "PG_USERNAME"
+  value         = var.pg_username
 }
--- github-repo/variables.tf
+++ github-repo/variables.tf
@@ -1,10 +1,10 @@
 variable "github_token" {}
 variable "github_reponame" { default = "coffee-api" }
 variable "github_branches" {
-    type = list(string)
-    default = [ "env/staging", "main" ]
+  type    = list(string)
+  default = ["env/staging", "main"]
 }
 variable "pg_password" {}
-variable "pg_username" { 
-    default = "postgres"
+variable "pg_username" {
+  default = "postgres"
 }
