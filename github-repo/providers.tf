df7729c0ecb1a4aca239390352ff7e424a785df9
Serhii Khylyk <sergey.hilik@gmail.com>
2024-08-06 12:13:45 +0200

    fix: add code to create repository

---
+++ github-repo/providers.tf
@@ -0,0 +1,15 @@
+terraform {
+    backend "local" {
+        path = "state/terraform.tfstate"
+  }
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "github" {
+  token = var.github_token # or `GITHUB_TOKEN`
+}
